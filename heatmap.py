6fe1c6e6441e62eda4a1ef38407ceee2dccbc559
Romy <rongyu_tang@163.com>
2024-04-18 10:14:21 +1000

    dpi fixed

--- heatmap.py
+++ heatmap.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 import flowkit as fk
 from matplotlib.patches import Polygon
-# from astropy.visualization.mpl_normalize import ImageNormalize
 from matplotlib.colors import LinearSegmentedColormap
-
 from PIL import Image
 
 from matplotlib import cm
@@ -16,7 +14,6 @@ from matplotlib.colors import LogNorm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# from torchinfo import summary
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data import Dataset, random_split
 import torch.optim as optim
@@ -26,7 +23,6 @@ import random
 import json
 import os
 import math
-
 from itertools import combinations
 
 scattering_channels = ['FSC-A', 'FSC-H', 'FSC-W', 'SSC-A', 'SSC-H', 'SSC-W']
@@ -45,17 +41,16 @@ def plot_heatmap(data, num, antibody_x, antibody_y, resolution=(80, 80)):
 
     bins = resolution
 
+    inches_per_bin = 10
     fig, ax = plt.subplots()
-    fig.set_size_inches(resolution[0] / 10, resolution[1] / 10)
+    fig.set_size_inches(resolution[0] / inches_per_bin, resolution[1] / inches_per_bin)
 
     data, x_e, y_e = np.histogram2d(x, y, bins=bins, density=True)
     z = interpn((0.5 * (x_e[1:] + x_e[:-1]), 0.5 * (y_e[1:] + y_e[:-1])), data, np.vstack([x, y]).T, method="splinef2d",
                 bounds_error=False)
 
-    # To be sure to plot all data
     z[np.where(np.isnan(z))] = 0.0
 
-    # Sort the points by density, so that the densest points are plotted last
     idx = z.argsort()
     x, y, z = x[idx], y[idx], z[idx]
 
@@ -66,7 +61,8 @@ def plot_heatmap(data, num, antibody_x, antibody_y, resolution=(80, 80)):
     ax.set_axis_off()
 
     output_filename = 'data' + str(num) + '_' + str(antibody_x) + '_' + str(antibody_y) + '.png'
-    fig.savefig(output_path + output_filename, bbox_inches='tight', pad_inches=0)
+    dpi = inches_per_bin * resolution[0] / 80
+    fig.savefig(output_path + output_filename, bbox_inches='tight', pad_inches=0, dpi=dpi)
     plt.close(fig)
 
 
